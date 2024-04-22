3d07d0e9e7fdd1ec32040b2a932809ff6af6a43c
rohan11202 <s.rohan11202@gmail.com>
2024-04-22 21:48:45 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
